dce6b01b86bb3821dd7c75f21c626e1f81af9684
Romario Raffington <romarioraff@gmail.com>
2017-09-30 07:43:25 -0500

    feat: Added getMetricMetaInfo helper method

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,8 @@
+import React from 'react'
+import { View } from 'react-native'
+import { FontAwesome, MaterailIcons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { white } from './colors';
+
 export function isBetween (num, x, y) {
   if (num >= x && num <= y) {
     return true
@@ -38,4 +43,93 @@ export function timeToString (time = Date.now()) {
   const date = new Date(time)
   const todayUTC = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
   return todayUTC.toISOString().split('T')[0]
+}
+
+export function getMetricMetaInfo (metric) {
+  const info = {
+    run: {
+      displayName: 'Run',
+      max: 50,
+      unit: 'miles',
+      step: 1,
+      type: 'steppers',
+      getIcon() {
+        <View>
+          <MaterialIcons 
+            name='directions-run'
+            color={'black'}
+            size={35}
+          />
+        </View>
+      }
+    },
+    bike: {
+      displayName: 'Bike',
+      max: 100,
+      unit: 'miles',
+      step: 1,
+      type: 'steppers',
+      getIcon() {
+        <View>
+          <MaterialCommunityIcons 
+            name='bike'
+            color={'black'}
+            size={35}
+          />
+        </View>
+      }
+    },
+    swim: {
+      displayName: 'Swim',
+      max: 9900,
+      unit: 'meters',
+      step: 100,
+      type: 'steppers',
+      getIcon() {
+        <View>
+          <MaterialCommunityIcons 
+            name='swim'
+            color={'black'}
+            size={35}
+          />
+        </View>
+      }
+    },
+    sleep: {
+      displayName: 'Sleep',
+      max: 24,
+      unit: 'hours',
+      step: 1,
+      type: 'slider',
+      getIcon() {
+        <View>
+          <FontAwesome 
+            name='bed'
+            color={'black'}
+            size={35}
+          />
+        </View>
+      }
+    },
+    eat: {
+      displayName: 'Eat',
+      max: 10,
+      unit: 'rating',
+      step: 1,
+      type: 'slider',
+      getIcon() {
+        <View>
+          <MaterialCommunityIcons 
+            name='food'
+            color={'black'}
+            size={35}
+          />
+        </View>
+      }
+    }
+  }
+
+  return typeof metric === 'undefined'
+    ? info
+    : info[metric]
 }
