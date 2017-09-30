6d6419eb86a3f9e69c07ae644e1cf5542da5840d
Romario Raffington <romarioraff@gmail.com>
2017-09-30 11:24:11 -0500

    feat: Added Touchable components and icons

--- components/FitnessStepper/index.js
+++ components/FitnessStepper/index.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { FontAwesome, Entypo } from '@expo/vector-icons'
 
-export default class FitnessStepper extends Component {
-  render () {
-    return (
-      <View>
-        <Text>FitnessStepper</Text>
-      </View>
-    )
-  }
-}
+export default ({ max, unit, step, value, onIncrement, onDecrement }) => (
+ <View>
+  <View>
+      <TouchableOpacity onPress={onIncrement}>
+        <FontAwesome name='plus' size={30} color={'black'} />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={onDecrement}>
+        <FontAwesome name='minus' size={30} color={'black'} />
+      </TouchableOpacity>
+    </View>
+    <View>
+      <Text>{value}</Text>
+      <Text>{unit}</Text>
+    </View>
+  </View>
+)
