38ab00fdcfad40571bcb63f379d210757b5f39e3
Romario Raffington <romarioraff@gmail.com>
2017-10-03 13:18:18 -0500

    stye: Added style for the TextButton component

--- components/TextButton/index.js
+++ components/TextButton/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
-import { TouchableOpacity, Text } from 'react-native'
+import { TouchableOpacity, Text, StyleSheet } from 'react-native'
 
 export default({ children, onPress}) => (
-  <TouchableOpacity onPress={onPress}>
+  <TouchableOpacity style={styles.button} onPress={onPress}>
     <Text> {children} </Text>
   </TouchableOpacity>
 )
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 10
+  }
+})
