1f3a0143327b02d599f336d46b863765bd1c3c67
Romario Raffington <romarioraff@gmail.com>
2017-10-04 20:26:44 -0500

    styles: Added styles for the Slider

--- components/FitnessSlider/index.js
+++ components/FitnessSlider/index.js
@@ -1,18 +1,36 @@
+// External Depedencies
 import React from 'react'
-import { View, Text, Slider } from 'react-native'
+import { View, Text, Slider, StyleSheet } from 'react-native'
+
+// Our Dependencies
+import { gray } from '../../utils/colors'
 
 export default ({ max, unit, step, value, onChange}) =>  (
-  <View>
+  <View style={styles.row}>
     <Slider 
+      style={{ flex: 1 }}
       step={step}
       value={value}
       maximumValue={max}
       minimumValue={0}
       onValueChange={onChange}
     />
-    <View>
-      <Text>{value}</Text>
-      <Text>{unit}</Text>
+    <View style={styles.metricCounter}>
+      <Text style={{ fontSize: 24, textAlign: 'center' }}>{value}</Text>
+      <Text style={{ fontSize: 18, color: gray }}>{unit}</Text>
     </View>
   </View>
 )
+
+const styles = StyleSheet.create({
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  metricCounter: {
+    width: 85,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
