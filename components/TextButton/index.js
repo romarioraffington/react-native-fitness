92ffa4e416fd483a01cc6c19d533be23543b59f4
Romario Raffington <romarioraff@gmail.com>
2017-10-04 20:27:19 -0500

    styles: Added styles for the TextButton

--- components/TextButton/index.js
+++ components/TextButton/index.js
@@ -1,14 +1,20 @@
+// External Dependencies
 import React from 'react'
 import { TouchableOpacity, Text, StyleSheet } from 'react-native'
 
-export default({ children, onPress}) => (
+// Our Dependencies
+import { purple } from '../../utils/colors'
+
+export default({ children, onPress, style = {} }) => (
   <TouchableOpacity style={styles.button} onPress={onPress}>
-    <Text> {children} </Text>
+    <Text style={[ styles.button, style ]}> {children} </Text>
   </TouchableOpacity>
 )
 
 const styles = StyleSheet.create({
   button: {
-    padding: 10
+    padding: 10,
+    color: purple,
+    fontSize: 20,
   }
 })
