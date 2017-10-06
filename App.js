d7093d847827ed471192fd62cef8ac2ae5f7c060
Romario Raffington <romarioraff@gmail.com>
2017-10-05 21:43:20 -0500

    feat: Added tabbed navigation

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 // External Components
 import React from 'react'
-import { View } from 'react-native'
+import { View, Platform } from 'react-native'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { TabNavigator } from 'react-navigation'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import devToolsEnhancer from 'remote-redux-devtools';
+
+// Our Depdencies
+import { purple, white } from './utils/colors'
 
 // Our Components
 import AddEntry from './components/AddEntry'
@@ -12,6 +17,40 @@ import History from './components/History'
 // Our Reducers
 import reducer from './reducers' 
 
+const Tabs = TabNavigator({
+  History: {
+    screen: History,
+    navigationOptions: {
+      tabBarLabel: 'History',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    }
+  },
+  AddEntry: {
+    screen: AddEntry,
+    navigationOptions: {
+      tabBarLabel: 'Add Entry',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    },
+  }
+}, {
+  navigationOptions: {
+    header: null,
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios'? purple: white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white: purple,
+      shadowColor: 'rgba(0,0,0,0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1,
+    }
+  }
+})
 // Boostrap store
 const store = createStore(reducer, devToolsEnhancer());
 
@@ -19,7 +58,7 @@ export default () => (
   <Provider store={store}>
     <View style={{flex: 1}}>
       <View style={{height: 20}}/>
-      <History />
+      <Tabs />
     </View>
   </Provider>
 )
