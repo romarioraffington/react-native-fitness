90f7fafb177bead573842d077b7f97abd32ba54d
Romario Raffington <romarioraff@gmail.com>
2017-10-10 23:46:19 -0500

    feat: Added animation to the direction

--- components/Live/index.js
+++ components/Live/index.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react'
 import { Location, Permissions } from 'expo'
 import { Foundation } from '@expo/vector-icons'
-import { View, Text, ActivityIndicator, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, ActivityIndicator, StyleSheet, TouchableOpacity, Animated } from 'react-native'
 
 // Our Dependencies
 import { calculateDirection } from '../../utils/helpers'
@@ -12,7 +12,8 @@ export default class Live extends Component {
   state = {
     coords: null,
     status: null,
-    direction: ''
+    direction: '',
+    bounceValue: new Animated.Value(1),
   }
 
   componentDidMount() {
@@ -49,17 +50,25 @@ export default class Live extends Component {
       distanceInterval: 1,
     }, ({ coords }) => {
       const newDirection = calculateDirection(coords.heading)
+      const { direction, bounceValue} = this.state
+
+      if (newDirection !== direction ) {
+        Animated.sequence([
+          Animated.timing(bounceValue, { duration: 200, toValue: 1.04 }),
+          Animated.spring(bounceValue, { toValue: 1, friction: 4 })
+        ]).start()
+      }
 
       this.setState({
         coords,
         status: 'granted',
-        direction: newDirection
+        direction: newDirection,
       })
     })
   }
 
   render() {
-    const { status, coords, direction } = this.state
+    const { status, coords, direction, bounceValue } = this.state
 
     if (status === null) {
       return <ActivityIndicator style={{ margin: 30 }} />
@@ -91,7 +100,9 @@ export default class Live extends Component {
             { 
               direction === 'Calculating'
               ?  <ActivityIndicator style={{ margin: 30 }} />  
-              :  <Text style={styles.direction}>{ direction } </Text>
+              : <Animated.Text style={[styles.direction, { transform: [{ scale: bounceValue }]}]}>
+                  { direction }
+                </Animated.Text>
             }
         </View>
         <View style={styles.metricContainer}>
